89561dbeda7daaabd0cc07bb7d0fc9c2ab91383b
Camilo Sanchez <camilin87@gmail.com>
2019-03-16 12:05:04 -0400

    change the rds module a lil bit so that dependencies can be injected

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    rds: require('./lib/rds'),
+    rds: require('./lib/rds')(),
     elasticBeanstalk: require('./lib/elasticBeanstalk')(),
     lambda: require('./lib/lambda')()
 }
